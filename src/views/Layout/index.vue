824a6b5ff0725a8534b6667be1e70f1cff4c14ca
limengting <1443848358@qq.com>
2022-07-15 17:53:12 +0800

    完成布局页面路由的配置

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
+    <router-view></router-view>
     <!-- 底部样式 -->
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+        </template>
+        <span>首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span>
+        </template>
+        <span>视频</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span>
+        </template>
+        <span>问答</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span>
+        </template>
+        <span>我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.toutiao {
+  font-size: 0.58667rem;
+}
 </style>
